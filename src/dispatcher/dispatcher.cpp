08887a7394d886173634dc21f61aab48f7df969e
apayne185 <apayne.ieu2022@student.ie.edu>
2026-01-13 15:04:49 +0100

    MPI integration completed

--- src/dispatcher/dispatcher.cpp
+++ src/dispatcher/dispatcher.cpp
@@ -1,28 +1,50 @@
 /*Routes tasks, if too small tasks stay on CPU (avoids cost of moving to GPU), but otherwise
-routes to the HPC path*/
+routes to the HPC path - calculation logic*/
 
 #include "stack_types.h"
 #include <iostream>
+#include <mpi.h>
 
 StackResult route_workload(HybridWorkload& wl) {
     StackResult res; 
-    if (wl.num_qubits < 15) {         // logic for simulator 
-    // if (wl.num_qubits < 15 && !wl.requires_gpu) {
-        // std::cout << "[Dispatcher] Routing to local simulator..." << std::endl;
-        // return 0.0; 
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    int param_size = wl.parameters.size();
+    MPI_Bcast(&param_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank != 0) {
+        wl.parameters.resize(param_size);
+    }
+
+    // Shares numerical parameters - theta
+    MPI_Bcast(wl.parameters.data(), param_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (rank ==0){
+        std::cout << "[MASTER DISPATCHER] Distributing "<< wl.num_qubits << " qubits across " << size << " nodes." <<std::endl;
+    }
+
+    // Dispatch logic 
+    if (wl.num_qubits < 15) {         
         res.energy = -1.137;    //placeholder vqe energy
         res.execution_time = 0.002;   //secs 
         res.variance = 0.001;      //placeholder for noise
         res.success_msg = "Success";
         res.used_path = "Local Simulator"; 
+
     } else {   //logc for HPC
-        // std::cout << "[Dispatcher] Routing to HPC Stub (No CUDA detected)..." << std::endl;
-        // return 1.0; 
-        res.energy = -1.137;    //placeholder vqe energy
+        double local_energy = -1.137 / size;     //palceholder for now, splits across nodes
+        double global_energy = 0.0;
+
+        //sums results from all nodes back to node 0
+        MPI_Reduce(&local_energy, &global_energy, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        res.energy = global_energy;
         res.execution_time = 0.450;   // HPC latency included - placeholder
         res.variance = 0.001;      //placeholder for noise
         res.success_msg = "Success";
-        res.used_path = "HPC Cluster - GPU"; 
+        res.used_path = "MPI + CUDA Distribued"; 
     }
 
     return res; 
